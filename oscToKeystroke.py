ebfe35d4f90f611083fea43d1af633b550cc5635
David Hay <misterhay@gmail.com>
2017-04-11 22:09:43 -0600

    first working version

--- oscToKeystroke.py
+++ oscToKeystroke.py
@@ -1,13 +1,122 @@
 #!/usr/bin/env python
-import OSC # https://github.com/ptone/pyosc
+import OSC # https://github.com/ptone/pyosc from https://trac.v2.nl/wiki/pyOSC
 import pyautogui # http://pyautogui.readthedocs.io/en/latest/cheatsheet.html
+import time
+import threading
 
-server = OSC.OSCServer( ("localhost", 7110) )
-server.timeout = 0
-run = True
+keyboardShortcuts = [ # can't include  *?,[]{}#
+    'comma',
+    'opensquare',
+    'closesquare',
+    'openparenthesis',
+    'closeparenthesis',
+    'pound',
+    '"',
+    "'",
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '0',
+    '-',
+    '=',
+    '!',
+    '@',
+    'f1',
+    'f2',
+    'f3',
+    'f4',
+    'f5',
+    'f6',
+    'f7',
+    'f8',
+    'f9',
+    'f10',
+    'f11',
+    'f12',
+    '\\',
+    'backspace',
+    ';',
+    '.',
+    'space',
+    'up',
+    'down',
+    'left',
+    'right',
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+]
+
+receiveAddress = ('0.0.0.0', 9000) # allow it to receive from any IP on port 9000
+server = OSC.OSCServer(receiveAddress) # set up the OSC server
 
 def messageHandler(path, tags, args, source):
-    print path
-    print tags
+    keystroke = path.lstrip('/')
+    if keystroke == 'comma':
+        keystroke = ','
+    if keystroke == 'opensquare':
+        keystroke = '['
+    if keystroke == 'closesquare':
+        keystroke = ']'
+    if keystroke == 'openparenthesis':
+        keystroke = '('
+    if keystroke == 'closeparenthesis':
+        keystroke = ')'
+    if keystroke == 'pound':
+        keystroke = '#'
+    
+    if args == [1.0]:
+        pyautogui.typewrite(keystroke)
+    if args == [2.0]: # shift
+        pyautogui.hotkey('shift',keystroke)
+    if args == [3.0]: # alt
+        pyautogui.hotkey('alt',keystroke)
+    if args == [4.0]: # ctrl
+        pyautogui.hotkey('ctrl',keystroke)
+        
 
-server.addMsgHandler( "/user/1", user_callback)
+for i in range(len(keyboardShortcuts)):
+    server.addMsgHandler('/'+keyboardShortcuts[i], messageHandler)
+
+threadingServer = threading.Thread(target = server.serve_forever)
+threadingServer.start() # start the server thread we just defined
+print 'OSC receiving server is running, press ctrl-c to quit'
+
+try:
+    while 1: # almost the same as while True:
+        time.sleep(5) # do nothing here, just wait for ctrl-c
+
+except KeyboardInterrupt: # what to do if there's a ctrl-c
+    print 'Stopping the OSC receiving server...'
+    server.close()
+    threadingServer.join() # stop the thread
+    print 'Server stopped'
